42aeb3a4ba495a7e24ca2d5b508fcc45c1cca657
Alejandro Rodríguez Natal <rodrigueznatalandy@gmail.com>
2017-05-16 17:57:15 -0400

    Added Basics to the master branch
    Added barebones

---
+++ Json_generator.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+def fakePerson():
+    #create the JSON here
+    return
+
+def makeDictionary():
+
+    return
+
+def getInformation():
+
+    return
+
+
+def __main__():
+    
+    n = int(input(""))
+    
+    return
